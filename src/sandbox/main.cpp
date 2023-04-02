b75e7337ba94ca94ed51d93da417b3084d671b73
sugaim <50139852+sugaim@users.noreply.github.com>
2023-04-02 18:36:00 +0000

    setup devenv

---
+++ src/sandbox/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <seal/seal.h>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv) {
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
